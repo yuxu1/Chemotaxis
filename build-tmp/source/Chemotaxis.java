68923146cf1cb02556b6f0350a12e3df048223c3
yuxu1 <yufanx8@gmail.com>
2014-09-22 12:11:24 -0700

    Added code to Chemotaxis.pde and title to index.html/

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,6 +1,22 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
 Bacteria [] colony;
 
-void setup()
+public void setup()
 {
 
   size (500, 500);
@@ -12,7 +28,7 @@ void setup()
   }
 }
 
-void draw()
+public void draw()
 {
   background(0);
   for (int i=0; i<colony.length; i++)
@@ -36,7 +52,7 @@ class Bacteria
     g=(int)(Math.random()*256);
     b=(int)(Math.random()*256);
   }
-  void move()
+  public void move()
   {
     if (X<=mouseX && Y<=mouseY)
     {
@@ -56,10 +72,19 @@ class Bacteria
       Y=Y-(int)(Math.random()*8);
     }
   }
-  void show()
+  public void show()
   {
     fill(r, g, b, (int)(Math.random()*144));
     ellipse(X, Y, Width, Length);
   }
 }
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
